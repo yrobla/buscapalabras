0307a3c5dada962e7ac28c02c8d2f4dac4766197
Yolanda Robla <info@ysoft.biz>
2018-01-07 11:17:43 +0100

    Ultimas funcionalidades con reward video

--- buscapalabras/www/js/my-app.js
+++ buscapalabras/www/js/my-app.js
@@ -6,7 +6,6 @@ var mainView = myApp.addView('.view-main')
         
 //If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
-var dict = new Triejs();
 
 //var PREMIUM = 0;
 var PREMIUM = 0;
@@ -19,19 +18,40 @@ else {
 	var MAX_COMODINES=0;
 }
 
+var resultado = {
+		'letras': [],
+		'mascara': [],
+		'longitud': 0,
+		'mostrar_anuncio': false,
+}
+function initResultado()
+{
+	resultado['letras'] = [];
+	resultado['mascara'] = [];
+	resultado['longitud'] = 0;
+	resultado['mostrar_anuncio'] = false;
+}
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
 
+function preparaAnuncio() {
+	if (AdMob) {
+		AdMob.prepareRewardVideoAd({
+			adId: 'ca-app-pub-3102751369411439/5206345444',
+			autoShow: false
+		});			
+	}
+}
 function ajustaPatron() {
 	var longitud = $$("#longitud").val();
 	$$('#longitud').prop({
         'min': 3,
-        'max': MAX_PATRON
+        'max': 10
     });	
 	$$("#outputLongitud").val(longitud);
 	
-	for (var i=1;i<=MAX_PATRON;i++) {
+	for (var i=1;i<=10;i++) {
 		if (i<=longitud) {
 			$$("#mask"+i).show();
 		}
@@ -65,13 +85,13 @@ document.addEventListener('pageInit', function (e) {
     	
     	$$('#popup-letras').on('click', function () {
     		myApp.alert('<div class="page-content">Introduce hasta siete letras para formar tu palabra. '+
-    				'En la versión premium, introduce <b>?</b> para usar comodines</div>',
+    				'Introduce <b>?</b> para usar comodines. Necesitarás ser premium o visualizar un anuncio cuando utilices comodines.</div>',
     				'<div><i class="f7-icons size-22 color-black">help</i>&nbsp;Ayuda</div>');
     		});
     	
     	$$('#popup-largo').on('click', function () {
     		myApp.alert('<div class="page-content">Mueve el slider para seleccionar la longitud de la palabra a generar. '+
-    				'La longitud va desde tres caracteres hasta ocho (diez en la versión premium).</div>',
+    				'La longitud va desde tres caracteres hasta diez. Para longitudes 9 y 10, deberás ser premium o visualizar un anuncio.</div>',
     				'<div><i class="f7-icons size-22 color-black">help</i>&nbsp;Ayuda</div>');
     		});    	
 
@@ -96,11 +116,7 @@ document.addEventListener('pageInit', function (e) {
 
 
 function validarCaracteres() {
-	var resultado = {
-			'letras': [],
-			'mascara': [],
-			'longitud': 0
-	}
+	initResultado();
 	
 	var chars = $$("#chars").val();
 	if (chars.length<2 || chars.length>7) {
@@ -131,12 +147,14 @@ function validarCaracteres() {
     }
 	
 	if (num_comodines>MAX_COMODINES) {
-		myApp.alert("Solo puedes introducir "+MAX_COMODINES+" comodines", "Error");
-		return false;
+		resultado['mostrar_anuncio'] = true;
 	}
-    
+	
     // ahora validamos mascara
 	var longitud = $$("#longitud").val();
+	if (longitud>MAX_PATRON) {
+		resultado['mostrar_anuncio'] = true;
+	}
 
 	var fijas = 0;
     for (var i=1;i<=longitud;i++) {
@@ -162,68 +180,84 @@ function validarCaracteres() {
         
     resultado['longitud'] = longitud;
     
-    return resultado;
+    return true;
 }
 
-function generaCombinaciones() {
+function devuelveResultados() {
 	myApp.showPreloader('Cargando...')
 	
-    // leemos todas las letras y chequeamos que tengan un valor valido y concuerden en longitud
-    resultado = validarCaracteres();
-    var final_items = [];
-    
-    if (resultado) {
-    	$$.getJSON('http://buscapalabras.ysoft.biz/palabras',
-    			{'letras': resultado['letras'].join(''),
-    		      'mascara': resultado['mascara'].join(''),
-    		      'longitud': resultado['longitud']},
-    			function(data) {
-    		    	var chip_content = '';
-				    if (data['palabras'].length==0) {
-				    	chip_content = chip_content +
-				    		'<div class="chip green"><div class="chip-label">No hay palabras confirmadas</div></div>\n';				    	
-				    }
-				    else 
-				    {
-				    	for (var i=0;i<data['palabras'].length;i++) {
-				    		var palabra_final = replaceAll(data['palabras'][i], '#', 'ñ');
-				    		
-				    		chip_content = chip_content +
-				    			'<div class="chip green"><div class="chip-label">'+ palabra_final + '</div></div>\n';
-				    	}
-				    }				    
-				    $$("#chips_container_dict").html('<a href="#" class="link icon-only" id="popup-confirmadas"><i class="f7-icons size-15 color-black">help</i></a>&nbsp;'+chip_content);
-				    
-    		    	var chip_content = '';
-				    if (data['posibles'].length==0) {
-				    	chip_content = chip_content +
-				    		'<div class="chip blue"><div class="chip-label">No hay palabras opcionales</div></div>\n';				    	
-				    }
-				    else 
-				    {
-				    	for (var i=0;i<data['posibles'].length;i++) {
-				    		var palabra_final = replaceAll(data['posibles'][i], '#', 'ñ');
-				    		
-				    		chip_content = chip_content +
-				    			'<div class="chip blue"><div class="chip-label">'+ palabra_final + '</div></div>\n';
-				    	}
-				    }
-				    
-				    $$("#chips_container_possible").html('<a href="#" class="link icon-only" id="popup-posibles"><i class="f7-icons size-15 color-black">help</i></a>&nbsp;'+chip_content);
-				    
-				    myApp.hidePreloader();
-    			},
-    			function(error) {
-    				myApp.hidePreloader();
-    			}
-    	);
-    }
-    else {
-		myApp.hidePreloader();
-    	
-    }
+	$$.getJSON('http://buscapalabras.ysoft.biz/palabras',
+			{'letras': resultado['letras'].join(''),
+		      'mascara': resultado['mascara'].join(''),
+		      'longitud': resultado['longitud']},
+			function(data) {
+		    	var chip_content = '';
+			    if (data['palabras'].length==0) {
+			    	chip_content = chip_content +
+			    		'<div class="chip green"><div class="chip-label">No hay palabras confirmadas</div></div>\n';				    	
+			    }
+			    else 
+			    {
+			    	for (var i=0;i<data['palabras'].length;i++) {
+			    		var palabra_final = replaceAll(data['palabras'][i], '#', 'ñ');
+			    		
+			    		chip_content = chip_content +
+			    			'<div class="chip green"><div class="chip-label">'+ palabra_final + '</div></div>\n';
+			    	}
+			    }				    
+			    $$("#chips_container_dict").html('<a href="#" class="link icon-only" id="popup-confirmadas"><i class="f7-icons size-15 color-black">help</i></a>&nbsp;'+chip_content);
+			    
+		    	var chip_content = '';
+			    if (data['posibles'].length==0) {
+			    	chip_content = chip_content +
+			    		'<div class="chip blue"><div class="chip-label">No hay palabras opcionales</div></div>\n';				    	
+			    }
+			    else 
+			    {
+			    	for (var i=0;i<data['posibles'].length;i++) {
+			    		var palabra_final = replaceAll(data['posibles'][i], '#', 'ñ');
+			    		
+			    		chip_content = chip_content +
+			    			'<div class="chip blue"><div class="chip-label">'+ palabra_final + '</div></div>\n';
+			    	}
+			    }
+			    
+			    $$("#chips_container_possible").html('<a href="#" class="link icon-only" id="popup-posibles"><i class="f7-icons size-15 color-black">help</i></a>&nbsp;'+chip_content);
+			    
+			    myApp.hidePreloader();
+			},
+			function(error) {
+				myApp.hidePreloader();
+			}
+	);
 	
 }
+
+function generaCombinaciones() {	
+    // leemos todas las letras y chequeamos que tengan un valor valido y concuerden en longitud
+    result = validarCaracteres();
+    if (!result) return false;
+	
+	if (resultado['mostrar_anuncio']) {
+		AdMob.showRewardVideoAd();				
+		preparaAnuncio();
+		
+	}
+    var final_items = [];
+    
+    var permite_extra = false;
+    
+	if (!resultado['mostrar_anuncio']) {
+		devuelveResultados();
+	}
+}
+
+document.addEventListener('onAdPresent', function(data) {
+	if (data.adType == 'rewardvideo') {
+		devuelveResultados();
+	}
+});
+
 
 //Wait for PhoneGap to load
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -236,12 +270,16 @@ function onDeviceReady() {
     if (!PREMIUM) {
     	var admobid = {};
 		admobid = {
-			      banner: 'ca-app-pub-3102751369411439/9704247246'    	  
+			      banner: 'ca-app-pub-3102751369411439/9704247246'
 		}
 		
-		if(AdMob) AdMob.createBanner({
+		if(AdMob) {
+			AdMob.createBanner({
 			  adId: admobid.banner,
 			  position: AdMob.AD_POSITION.BOTTOM_CENTER,
-			  autoShow: true });		
+			  autoShow: true });
+			
+			preparaAnuncio();			
+		}
 	}
 }
